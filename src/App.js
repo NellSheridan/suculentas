20e83b9bca4f9f719b04059c14e38d03b7ab1af5
Nelida Quispe <nelida.quispetacuri@gmail.com>
2021-06-26 03:32:15 -0500

    feat(): Modifica estilos y corrige test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import pngegg from './pngegg.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pngegg} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://es.wikipedia.org/wiki/Suculenta"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SUCULENTAS
         </a>
       </header>
     </div>
