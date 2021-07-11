b0a87cef6ddc60ac3faf57eee4c7bcdb56487974
Nelida Quispe <nelida.quispetacuri@gmail.com>
2021-07-10 23:53:13 -0500

    feat(): Agrega contador

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+const ItemCount = (props) => {
+    const [count, setCount] = useState(1);
+
+    const buttonIncreaseQuantity = () => {
+        setCount(count + 1);
+    }
+
+    const buttonDecreaseQuantity = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <div className="item-count-wrapper">
+            <button disabled={count == props.min} onClick={buttonDecreaseQuantity}>-</button>
+            <span>{ count }</span>
+            <button disabled={count == props.max} onClick={buttonIncreaseQuantity}>+</button>
+        </div>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) => {
-    console.log(props);
     return (
         <main className="main-wrapper">
             <h1>{props.greeting.title}</h1>
@@ -10,6 +10,7 @@ const ItemListContainer = (props) => {
             <section>
                 {props.greeting.description}
             </section>
+            <ItemCount min={1} max={10}/>
         </main>
     )
 }
