3c49c25e65044aee19c82e3bab84cbb341693845
Nelida Quispe <nelida.quispetacuri@gmail.com>
2021-07-21 23:56:10 -0500

    feat(): Agrega router para manejar navegacion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemListContainer/ItemDetailContainer/ItemDetailContainer';
+
+import WelcomeContainer from './components/WelcomeContainer/WelcomeContainer';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   const message = () => alert('Esta sección esta en construccion!!')
   const greetings = {
-    title: '!Bienvenido¡',
+    title: '!Directo del Vivero te ofrecemos:¡',
     subtitle: 'Ponte cómodo :)',
     description: 'En éste espacio encontrarás inspiración relacionada con el mundo de las suculentas, cactus, y sus variantes. ¡Espero que te guste!'
   };
   return (
     <div className="App">
-      <NavBar eventClick={message}/>
-      <ItemListContainer greeting={greetings}/>
+      <BrowserRouter>
+        <NavBar eventClick={message}/>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeContainer />
+          </Route>
+          <Route path="/productos">
+            <ItemListContainer greeting={greetings}/>
+          </Route>
+          <Route path="/promociones">
+            <ItemDetailContainer />
+          </Route>
+          <Route path="/historia">
+            <ItemDetailContainer />
+          </Route>
+          <Route path="/bolsa">
+            <checkoutCart />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
