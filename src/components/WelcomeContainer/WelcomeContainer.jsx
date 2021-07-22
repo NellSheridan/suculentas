a25c22dbb202b4b3c1e0f0dbf2e5cb814826cc44
Nelida Quispe <nelida.quispetacuri@gmail.com>
2021-07-22 00:29:56 -0500

    feat(): Agrega estilos

--- src/components/WelcomeContainer/WelcomeContainer.jsx
+++ src/components/WelcomeContainer/WelcomeContainer.jsx
@@ -4,7 +4,16 @@ import './WelcomeContainer.css';
 const WelcomeContainer = () => {
     return (
         <div className="welcome-wrapper">
-            
+                <h1 className="title-welcome">
+                #Si acabas de llegar bienvenido, si ya te vas vuelve pronto
+                </h1>
+                <h2 className="title-secondary">
+                    Colecciones
+                    <br />
+                    Suculentas - Cactus y más
+                </h2>
+                <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tenetur enim aut laborum voluptas accusamus rerum error, dolorum, iste hic ducimus labore, nam provident eaque autem accusantium aliquid obcaecati impedit?</p>
+                <button className="btn-shop">Elige ahora</button>
         </div>
     )
 }
