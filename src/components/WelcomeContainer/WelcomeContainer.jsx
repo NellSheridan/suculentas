a2e298f32bdda1e957e8947f68a01f8a0df93f53
Nelida Quispe <nelida.quispetacuri@gmail.com>
2021-07-21 23:52:27 -0500

    feat(): Agrega componente de Bienvenida

---
+++ src/components/WelcomeContainer/WelcomeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WelcomeContainer.css';
+
+const WelcomeContainer = () => {
+    return (
+        <div className="welcome-wrapper">
+            
+        </div>
+    )
+}
+
+export default WelcomeContainer;
